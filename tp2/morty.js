be8d8f60b6cc555778608d769e847d13d63f2a16
marcos-reynoso <marcos_reynoso95@hotmail.com>
2023-05-11 01:13:20 -0300

    cambien el codigo usando el formato de promesas

--- tp2/morty.js
+++ tp2/morty.js
@@ -1,28 +1,54 @@
-const id = document.getElementById('id')
-const nombre = document.getElementById('nombre')
-const imagen = document.getElementById('imagen')
-const especie = document.getElementById('especie')
-const estatu = document.getElementById('estatu')
-const episodio = document.getElementById('episodio')
-
-function personaje(){
-console.log(id.value)
-fetch('https://rickandmortyapi.com/api/character/'+id.value) .then(resp =>
-resp.json()) .then(personajeresp =>{
-nombre.innerHTML=personajeresp.name
-especie.innerHTML=personajeresp.species
-estatu.innerHTML=personajeresp.status
-episodio.innerHTML=personajeresp.episode.length
-imagen.src=personajeresp.image
-})
+async function personaje() {
+  const id1 = document.getElementById("id1").value;
+  const id2 = document.getElementById("id2").value;
+  const [datos1, datos2] = await Promise.all([
+    obtenerdatospersonaje(id1),
+    obtenerdatospersonaje(id2),
+  ]);
+  actualizarelementoshtml(datos1, datos2);
 }
-
-
-
-
-
-
-
+async function obtenerdatospersonaje(id) {
+  const resultado = await fetch(
+    `https://rickandmortyapi.com/api/character/${id}`
+  );
+  const data = await resultado.json();
+  return {
+    nombre: data.name,
+    especie: data.species,
+    estado: data.status,
+    episodios: data.episode.length,
+    imagen: data.image,
+  };
+}
+function actualizarelementoshtml(datos1, datos2) {
+  const divDatos1 = document.getElementById("datos1");
+  const divDatos2 = document.getElementById("datos2");
+  divDatos1.innerHTML = `
+    <h2>Personaje 1</h2>
+    <p>${datos2.imagen}</p>
+    <p>Nombre: ${datos1.nombre}</p>
+    <p>Especie: ${datos1.especie}</p>
+    <p>Estado: ${datos1.estado}</p>
+    <p>Episodios: ${datos1.episodios}</p>
+  `;
+  divDatos2.innerHTML = `
+    <h2>Personaje 2</h2>
+    <p>${datos2.imagen}</p>
+    <p>Nombre: ${datos2.nombre}</p>
+    <p>Especie: ${datos2.especie}</p>
+    <p>Estado: ${datos2.estado}</p>
+    <p>Episodios: ${datos2.episodios}</p>
+  `;
+}
+if (datos1.episodios > datos2.episodios) {
+  alert(
+    `El  (${datos1.episodios}) es mayor que el segundo  (${datos2.episodios})`
+  );
+} else if (datos2.episodios > datos1.episodios) {
+  alert(`El (${datos2.episodios}) es mayor que el   (${datos1.episodios})`);
+} else {
+  alert("Ambos tienen la misma cantida de episodios ");
+}
 /* {
     "id": 5,
     "name": "Jerry Smith",
