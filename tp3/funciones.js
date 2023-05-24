e80687825e59dee186fab4bdf0f6afd648a7e091
marcos-reynoso <marcos_reynoso95@hotmail.com>
2023-05-24 19:16:01 -0300

    tp3

--- tp3/funciones.js
+++ tp3/funciones.js
@@ -1,39 +1,53 @@
-function randomuser() {
-  fetch("https://randomuser.me/api/")
-    .then((Response) => Response.json())
-    .then((datosr) => {
-      console.log(datosr);
-      mostrardatosr(datosr.results[0]);
-    });
+async function randomuser() {
+  const Response = await fetch("https://randomuser.me/api/");
+  const datosr = await Response.json();
+  return datosr.results[0];
 }
 
-function morty() {
+async function morty() {
   const random = Math.floor(Math.random() * 826);
-  fetch(`https://rickandmortyapi.com/api/character/${random}`)
-    .then((Response) => Response.json())
-    .then((datosm) => mostrardatosm(datosm));
+  const Response = await fetch(
+    `https://rickandmortyapi.com/api/character/${random}`
+  );
+  const datosm = await Response.json();
+  return datosm;
 }
 
 function mostrardatosm(datosm) {
   const rick = document.getElementById("morty");
   rick.innerHTML = `
-<img src="${datosm.image}" >
-<p> nombre: ${datosm.name} </p>
-<p> genero: ${datosm.gender} </p>
-`;
+  <div class="${datosm.gender.toLowerCase()}">
+  <img src="${datosm.image}" >
+  <p> nombre: ${datosm.name} </p>
+  <p> genero: ${datosm.gender} </p>
+  `;
 }
+
 function mostrardatosr(datosr) {
   const usario = document.getElementById("randomuser");
   usario.innerHTML = `
+  <div class="${datosr.gender}">
     <img src="${datosr.picture.large}" >
     <p> nombre: ${datosr.name.title} ${datosr.name.first}  ${datosr.name.last}</p>
     <p> genero: ${datosr.gender} </p>
     <p> dni: ${datosr.id.value} </p>
     <p> latitud: ${datosr.location.coordinates.latitude} </p>
     <p> longitud: ${datosr.location.coordinates.longitude} </p>
-  `;
+    `;
 }
-function buscar() {
-  randomuser();
-  morty();
+function categoriacomun(randomuser, morty) {
+  const resultado = document.getElementById("resultado");
+  if (randomuser.toLowerCase() === morty.toLowerCase()) {
+    resultado.innerHTML = `match`;
+  } else {
+    resultado.innerHTML = `no match`;
+  }
+}
+
+async function buscar() {
+  const datosr = await randomuser();
+  const datosm = await morty();
+  mostrardatosm(datosm);
+  mostrardatosr(datosr);
+  categoriacomun(datosr.gender, datosm.gender);
 }
